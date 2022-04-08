573d052c9f4f1401e3a291f2f2ff7ada5f6f1a61
Terfa <terphadaniel@gmail.com>
2022-04-08 14:39:54 -0700

    new commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   **     */
-
+* main - Entry point
+* Return: always 0 (success)
+*/
 int main(void)
-
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (success)
- *
- *     */
-
+ * main -entry point
+ * Return: always 0(success)
+ */
 int main(void)
-
 {
 int a;
 long int b;
@@ -20,6 +12,8 @@ char d;
 float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));       	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c);											printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c);
+printf("Size of a float: %lu bytes(s)", (unsigned long)sizeof(f));
 return (0);
 }
