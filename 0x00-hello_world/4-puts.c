f7e4034d10e043c009c4da17afab78d485c30bcc
Terfa <terphadaniel@gmail.com>
2022-04-08 11:36:17 -0700

    new solutions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   **     */
+
+int main(void)
+
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
