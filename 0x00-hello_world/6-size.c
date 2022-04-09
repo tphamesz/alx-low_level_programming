f97d2b45491f34dbab2e18dabde996a4a6ea13a2
Terfa <terphadaniel@gmail.com>
2022-04-09 15:50:39 -0700

    commits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,14 +5,12 @@
  * description: displays size of various data types of the machine it is compil
  * Return: always 0(success)
  */
-
 int main(void)
 {
-printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
-printf("Size of an int: %i byte(s)\n", (int) sizeof(int));
-printf("Size of a long int: %i bytes(s)\n", (int) sizeof(long int));
-printf("Size of a long long int: %i byte(s)\n", (int) sizeof(long long int));
-printf("Size of a float: %i bytes(s)", (int) sizeof(float));
-
-return (0);
+   printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
+   printf("Size of an int: %i byte(s)\n", (int) sizeof(int));
+   printf("Size of a long int: %i bytes(s)\n", (int) sizeof(long int));
+   printf("Size of a long long int: %i byte(s)\n", (int) sizeof(long long int));
+   printf("Size of a float: %i bytes(s)", (int) sizeof(float));
+   return (0);
 }
