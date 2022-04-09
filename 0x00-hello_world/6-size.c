e806dbe6658345221f9dc29a925dfe5459f32bc8
Terfa <terphadaniel@gmail.com>
2022-04-08 17:39:08 -0700

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@ int main(void)
 {
 printf("Size of a char: %i byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
-printf("size of a long int: %i bytes(s)\n", sizeof(long int));
+printf("Size of a long int: %i bytes(s)\n", sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 printf("Size of a float: %i bytes(s)", sizeof(float));
 return (0);
