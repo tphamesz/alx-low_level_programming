190c52ea85ed3b7a4efbf7ac46c1c0da606c9d2e
Terfa <terphadaniel@gmail.com>
2022-04-15 20:27:54 -0700

    code to print diagonal lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *print_diagonal - draws a diagonal line on the terminal
-*@n: number of times the character \ should be printed
+* @n: number of times the character \ should be printed
 */
 void print_diagonal(int n)
 {
@@ -13,12 +13,14 @@ else
 {
 int i, j;
 for (i = 0; i < n; i++)
+{
 for (j = 0; j < n; j++)
 {
 if (j == i)
 _putchar('\\');
 else if (j < i)
 _putchar(' ');
+}
 _putchar('\n');
 }
 }
