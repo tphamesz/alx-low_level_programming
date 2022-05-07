6f273c411ebddde939a9d3f58d97547a9107e1ef
Terfa <terphadaniel@gmail.com>
2022-05-07 12:31:11 -0700

    many solutions

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_islower - check main
+*@c: An input character
+*Description: function uses _putchar function to print
+*alphabet in lowercase 10 times
+*Return: 1 if it is lowercase and 0 if it  is uppercase
+*/
+int _islower(int c)
+{
+char i;
+int lower = 0;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i == c)
+lower = 1;
+}
+return (lower);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs - check main
+*@r: An integer input
+*Description: this funcion returns the absolute value of a number
+*Return: absolute value of a number r
+*/
+int _abs(int r)
+{
+if (r >= 0)
+return (r);
+else
+return (r * -1);
+}
